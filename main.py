cf31a5ae247e081f027f92724e34615cb08e3e9d
umidjanovnaa <sheraliyevaruxshona08@gmail.com>
2023-11-30 03:10:06 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#1-vazifa
+a = int(input("Juft son kiriting: "))
+if a//2:
+    print("Raxmat!")
+else:
+    print("Bu juft son emas!")
+
+#2-masala
+    age = int(input("Sizning yoshingiz: "))
+    if (age < 4) + (age > 60):
+        print("Agar foydalanuvchi 4 yoshdan kichik yoki 60 dan katta bolsa bepul!")
+    elif (age < 18):
+        print("Agar foydalanuvchi 18 dan kichik bo'lsa 10000 so'm")
+    elif (age > 18):
+        print("Agar foydalanuvchi 18 dan katta bo'lsa 20000 so'm")
